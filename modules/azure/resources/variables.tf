233b22b3847ecefb2eacf3631efc92e1e657db93
mo <mo3789530@gmail.com>
2023-12-10 21:38:56 +0900

    add resource group

---
+++ modules/azure/resources/variables.tf
@@ -0,0 +1,14 @@
+variable "location" {
+  description = "The Azure Region where the Resource Group should exist. Changing this forces a new Resource Group to be created."
+  type        = string
+}
+
+variable "name" {
+  description = "The Name which should be used for this Resource Group. Changing this forces a new Resource Group to be created."
+  type        = string
+}
+
+variable "tags" {
+  description = "A mapping of tags which should be assigned to the Resource Group."
+  type        = map(string)
+}
